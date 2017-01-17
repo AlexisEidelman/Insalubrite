c7f87c667ed7785018b7467cef5f1dd40ee37843
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2017-01-17 17:19:32 +0100

    parse sql

--- insalubrite/Sarah/read.py
+++ insalubrite/Sarah/read.py
@@ -7,7 +7,7 @@ Created on Fri Jan  6 14:49:42 2017
 import os
 import pandas as pd
 
-from insalubrite.config_insal import path_sarah
+from insalubrite.config_insal import path_sarah, path_sql_sarah
 
 
 def read_table(name):
@@ -28,9 +28,42 @@ def read_table(name):
 
 def read_sql():
     ''' return a dict'''
+    primary_key = dict()
+    foreign_key = list()
+    
+    with open(path_sql_sarah) as sql:
+        for line in sql.readlines():
+            if line.startswith('--'):
+                continue
+            if 'create table' in line:
+                name_table = line.strip().split(' ')[2]
+            if 'primary key' in line:
+                key = line.strip().split(' ')[2][1:-1]
+                if key[-1] == ')':
+                    key = key[:-1]
+                primary_key[name_table] = key
+
+            # alter
+        #        alter table ADRBAD 
+        #        add constraint FK72CF84F676DF198E 
+        #        foreign key (PARCELLE_ID) 
+        #        references PARCELLE_CADASTRALE;
+            if 'alter table' in line:
+                name_table_alter = line.strip().split(' ')[2]
+            if 'foreign key' in line:
+                key_alter = line.strip().split(' ')[2][1:-1]
+            if 'references' in line:
+                reference_alter = line.strip().split(' ')[1][:-1]
+                foreign_key += [(name_table_alter,key_alter, reference_alter)]
+                print(name_table_alter,key_alter, reference_alter)
+
+
+    return primary_key, foreign_key   
+    
     
 
 if __name__ == '__main__':
-    tab = read_table('affaire')
-    print(tab.head())
-    print(tab.dtypes)
+    bbb = read_sql()
+#    tab = read_table('affaire')
+#    print(tab.head())
+#    print(tab.dtypes)
