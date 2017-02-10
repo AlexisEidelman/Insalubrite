9bd680599c96487290b098e9638d712ce218abbe
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2017-02-10 12:59:24 +0100

    answer bien_id

--- insalubrite/Sarah/questions.py
+++ insalubrite/Sarah/questions.py
@@ -5,7 +5,7 @@ Created on Thu Feb  9 13:24:09 2017
 """
 
 
-#### 
+####
 #On regarde si une affaire de la table affaire et ou bien dans
 #aff_hygiene ou bien dans aff_ravalement
 ####
@@ -31,3 +31,28 @@ assert(all(~rava['affaire_id'].isin(hyg['affaire_id'])))
 # les affaires sont bien dans affhygiene et dans affravalement
 liste_affaire = (rava['affaire_id'].append(hyg['affaire_id'])).tolist()
 assert(all(affaire['id'].isin(liste_affaire)))
+
+
+####
+## Question sur bien_id
+####
+hyg.bien_id
+
+# on va regarder les id de toutes les tables et
+# selectionner celle qui contiennent les valeur de bien_id
+import os
+
+primary_key, foreign_key = read_sql()
+tables_on_disk = set(x[:-4] for x in os.listdir(path_sarah))
+
+potentiel_match = []
+for name in tables_on_disk:
+    tab = read_table(name, nrows=0)
+    if 'id' in tab.columns:
+        tab = read_table(name)
+        id_tab = tab['id']
+        if all(hyg.bien_id.isin(id_tab)):
+            potentiel_match.append(name)
+
+print(potentiel_match)
+# return ['ficherecolem']
