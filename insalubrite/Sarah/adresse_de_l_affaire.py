e85def80fedbbe6b696c955a65e2917da7ed46ca
KevinKpakpo <adkevinkpakpo@gmail.com>
2017-03-28 18:13:53 +0200

    clean adresse (wip)

--- insalubrite/Sarah/adresse_de_l_affaire.py
+++ insalubrite/Sarah/adresse_de_l_affaire.py
@@ -2,36 +2,30 @@
 """
 Created on Fri Mar 24 12:15:44 2017
 
+TODO: faire le lien entre adrbad, voie, parcelle
+TODO: faire la séléction des variable au fur et à mesure
 
 """
-from insalubrite.Sarah.read import read_table
 import pandas as pd
 
-affhygiene = read_table('affhygiene')
+from insalubrite.Sarah.read import read_table
 
-lien_signalement_affaire = read_table('signalement_affaire')
-
-sign = read_table('signalement')
-
-sign.adresse_id
 
 ### éude des adresses 
-adrbad = read_table('adrbad')
-del adrbad['id_bad'] #id_bad est 
 
-adrsimple = read_table('adrsimple')
 
 adresse = read_table('adresse')
-
-#TODO: est-ce que les adresses sont ou bien dans adrbad ou bien dans adrsimple ?
-
-
+# est-ce que les adresses sont ou bien dans adrbad ou bien dans adrsimple ?
+# vu la tête de adresse on peut supposer que oui
+adrsimple = read_table('adrsimple')
 
 # lien vers la parcelle, la voie et l'adresse
 voie = read_table('voie')
-adrbad.suffixe3.value_counts()
 
-#Merge tables
+
+# Merge tables
+affhygiene = read_table('affhygiene')
+lien_signalement_affaire = read_table('signalement_affaire')
 result1 = pd.merge(affhygiene, lien_signalement_affaire, on='affaire_id')
 ##len(affhygiene.affaire_id)
 ##37322
@@ -39,25 +33,28 @@ result1 = pd.merge(affhygiene, lien_signalement_affaire, on='affaire_id')
 ##30871
 ##len(result1.affaire_id)
 ##30692
-sign = read_table('signalement')
+signalement = read_table('signalement')
+signalement = signalement[['id', 'observations', 'adresse_id']]
 ##Rename 'id' column of signalement table
-sign['signalement_id'] = sign['id']
-del sign['id']
+signalement['signalement_id'] = signalement['id']
+del signalement['id']
 
-result2 = pd.merge(result1, sign, on='signalement_id')
+result2 = pd.merge(result1, signalement, on='signalement_id')
 ##len(result1.signalement_id)
 ##30692
 ##len(sign.signalement_id)
 ##36080
 ##len(result2.signalement_id)
 ##30692
-result = pd.merge(result2, adrsimple, on='adresse_id')
-##len(result2.adresse_id)
-##30692
-##len(adrsimple.adresse_id)
-##95461
-##len(result.adresse_id)
-##239
+
+adrbad = read_table('adrbad')
+del adrbad['id_bad'] #id_bad est 
+result = pd.merge(result2, adrbad, on='adresse_id')
+len(result2) # => 30692
+len(adrbad)  # => 146306
+len(result)  # => 30453
+# Les 239 qui ne sont pas matché avec adrbad sont marché avec adrsimple
+#TODO: faire le matching avec adrsimple pour les 239 cas
 
 ## 68 features: can we drop some of them?
 result  = result[['affaire_id', 'bien_id','designation','libelle_adresse','ville_adresse',
