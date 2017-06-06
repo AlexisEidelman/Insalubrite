5ecf99e52ed3bb42087858a68d26388abecb9491
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2017-06-06 11:50:34 +0200

    split_by_date

--- insalubrite/analyse/split.py
+++ insalubrite/analyse/split.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 On crée ici différentes façon de spliter un sujet
+
+Il retourne comme train_test_split de sickit-learn
+un quadruplet : X_train, X_test, y_train, y_test
 """
 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def simple_split(tab, train_size=0.75):
@@ -13,3 +17,30 @@ def simple_split(tab, train_size=0.75):
                                                         train_size=train_size,
                                                         )
 
+def split_by_date(tab, serie_date, train_size=0.75):
+    assert isinstance(serie_date, str)
+    assert serie_date in tab.columns
+    
+    nb_train_rows = int(len(tab)*train_size)
+    out_tab = tab.sort_values('date')
+    
+    train = out_tab.iloc[:nb_train_rows]
+    test = out_tab.iloc[nb_train_rows:]
+    return (train.drop(['est_insalubre'], axis=1),
+            test.drop(['est_insalubre'], axis=1),
+            train['est_insalubre'],
+            test['est_insalubre'],
+            )
+            
+    
+
+if __name__ == '__main__':    
+    from data import get_data
+    import preprocess
+    
+    tab_ini = get_data('batiment')    
+    tab = preprocess.keep_cols_for_analysis(tab_ini)
+    float_tab = preprocess.to_float(tab)
+    
+    float_tab['date'] = pd.to_datetime(tab_ini['date_creation'])
+    a, b, c, d = split_by_date(float_tab, "date")
